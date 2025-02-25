a930ef3ace3824406bba6a7bae28a7c024c27043
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2025-02-25 23:14:20 +0800

    Create WiFiPwCracker.py

---
+++ WiFiPwCracker.py
@@ -0,0 +1,50 @@
+import random
+import time
+import pywifi
+from pywifi import const
+
+def auto_guess(target):
+    
+    attempts = 0
+    guess = "00000000"
+    
+    while guess != target:
+        attempts += 1
+        guess = str(random.randint(10**7, 10**8 - 1))
+        if guess[6] == '0':
+            continue
+        
+    print(f"找到目標數字: {target}，總共嘗試次數: {attempts}")
+
+def connect_wifi(target_ssid, target_password):
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[0]
+
+    while True:
+        iface.disconnect()
+        time.sleep(1)
+
+        profile = pywifi.Profile()
+        profile.ssid = target_ssid
+        profile.auth = const.AUTH_ALG_OPEN
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        profile.cipher = const.CIPHER_TYPE_CCMP
+        profile.key = target_password
+
+        iface.remove_all_network_profiles()
+        tmp_profile = iface.add_network_profile(profile)
+
+        iface.connect(tmp_profile)
+        print(f"嘗試連接到 {target_ssid}...")
+        time.sleep(5)
+
+        if iface.status() == const.IFACE_CONNECTED:
+            print(f"成功連接到 WiFi: {target_ssid}")
+            break
+        else:
+            print(f"無法連接到 WiFi: {target_ssid}，1秒後重試...")
+
+if __name__ == '__main__':
+    ssid = "Your_WiFi_SSID"
+    password = "Your_WiFi_Password"
+    connect_wifi(ssid, password)
